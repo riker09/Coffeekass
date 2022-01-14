e7325086c350445e1f8afb4464c7daf26327e456
Volker Thiel <568497+riker09@users.noreply.github.com>
2022-01-14 10:45:40 +0100

    Add @types/express package

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const getProducts = functions.https.onRequest((request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  response.setHeader('Content-Type', 'application/json');
+  const products: unknown[] = [];
+  response.send(products);
+});
