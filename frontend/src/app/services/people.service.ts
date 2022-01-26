670ac0c8902ee8df9ac3f738362f3dd81d3cf054
Volker Thiel <568497+riker09@users.noreply.github.com>
2022-01-26 12:52:27 +0100

    Add people service & implement checkout in cart

---
+++ frontend/src/app/services/people.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Person } from '../../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+
+  private _people: Array<Person & Partial<{ id: string }>> = [];
+  private _selectedPerson?: Person;
+
+  constructor(
+    private firestore: Firestore,
+  ) {
+    const coll = collection(this.firestore, 'person');
+    collectionData(coll, {
+      idField: 'id',
+    }).subscribe((x) => {
+      this._people = x as Array<Person & Partial<{ id: string }>>;
+    });
+  }
+
+  get people () {
+    return this._people;
+  }
+
+  get selectedPerson () {
+    return this._selectedPerson;
+  }
+
+  get hasSelectedPerson () {
+    return !!this._selectedPerson;
+  }
+
+  selectPerson (person?: Person) {
+    this._selectedPerson = person;
+  }
+
+}
