4da9a62b8eabb783d15f2314c5e49342ae1879f1
Volker Thiel <568497+riker09@users.noreply.github.com>
2022-01-26 13:43:15 +0100

    Store time of purchase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,13 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 import * as cors from 'cors';
 import { purchase } from "./purchase";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
 const corsMiddleware = cors({ origin: true });
 
-export const getProducts = functions.https.onRequest((request, response) => {
-  corsMiddleware(request, response, () => {
-    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    response.setHeader('Content-Type', 'application/json');
-    const products: Array<{ name: string, price: number }> = [{
-      name: 'Wasser',
-      price: 0,
-    }, {
-      name: 'Kaffee',
-      price: 0.5,
-    }];
-    response.send(products);
+export const helloWorld = functions.https.onRequest((req, res) => {
+  corsMiddleware(req, res, async () => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    res.send("Hello from Firebase!");
   });
 });
 
