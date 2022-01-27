aaefaa5eb8f87cac6e47c1c26865637002e94bab
Volker Thiel <568497+riker09@users.noreply.github.com>
2022-01-27 09:53:55 +0100

    Add PrimeVue, PrimeFlex, PrimeIcons

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <Button @click="count++">count is: {{ count }}</Button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -34,19 +34,4 @@ const count = ref(0)
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('Button', Button);
+
+app.mount('#app')
