f6285982ff52fedad37264a236e28e1cd394f7f1
Volker Thiel <568497+riker09@users.noreply.github.com>
2022-02-02 08:44:29 +0100

    Add Firebase configuration to production env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyDr8yqzzFmfVbFgQhH8G0G5t7RLoABpJ8M",
+    authDomain: "coffeekass.firebaseapp.com",
+    projectId: "coffeekass",
+    storageBucket: "coffeekass.appspot.com",
+    messagingSenderId: "668079708561",
+    appId: "1:668079708561:web:60fb643b79ef5790536230",
+  },
 };
